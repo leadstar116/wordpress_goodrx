f863e61b521843e20dbbd04d3812515bb968387c
leadstar116 <supremedev116@gmail.com>
2020-04-11 23:59:28 +1000

    goodrxplugin initialize

---
+++ wp-content/plugins/goodrxplugin/goodrxplugin.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name:  GoodRXPlugin
+Description:  Custom plugin for GoodRX site.
+Plugin URI:   https://profiles.wordpress.org/leadstar116/
+Author:       Leadstar116
+Version:      1.0
+Text Domain:  goodrxplugin
+Domain Path:  /languages
+License:      GPL v2 or later
+License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+
+// exit if file is called directly
+if ( ! defined( 'ABSPATH' ) ) {
+
+	exit;
+
+}
